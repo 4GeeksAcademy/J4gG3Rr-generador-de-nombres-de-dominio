af25cbf0cb5b7a4217fdf99f473d884f4cbd08e9
Johan Gomez <johangomez0612@gmail.com>
2025-07-03 01:21:14 +0000

    generador de dominios terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  pronoun.forEach(function (pro) {
+    adj.forEach(function (adjetive) {
+      noun.forEach(function (sus) {
+
+        console.log(`${pro}${adjetive}${sus}.com`);
+      });
+    });
+  });
+
+
+
+}
